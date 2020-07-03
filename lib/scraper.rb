d1ce5713846e5c361ddb5a1a9e3546cbd6ea1cf3
Tomas Perez <tomasitopg6@gmail.com>
2020-07-03 08:17:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text
-
+##
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
  
